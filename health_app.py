88dd081442702cea2ec4677f2a55a2f839d27ebc
KurMaciej <kurowskimaciej11@gmail.com>
2024-07-27 14:45:23 +0200

    Preparing data

--- health_app.py
+++ health_app.py
@@ -3,10 +3,16 @@ import os
 
 if __name__ == "__main__":
     curr_dir = os.getcwd()
-    #Need to skip first line from the file
     file_name = 'com.samsung.shealth.activity.day_summary.20240713114102.csv'
-
+    col_to_load = [4, 10, 12, 22, 24]
     file_path = curr_dir + '\\s_health\\' + file_name 
 
-    df = pd.read_csv(file_path, skiprows = 1)
+    #Read file (skip first line from the file)
+    df = pd.read_csv(file_path, skiprows = 1, usecols = col_to_load)
+
+    #Delete rows where steps = 0 
+    df_series = df['step_count'] != 0
+    df = df[df_series]
+
+    print(df.columns)
     print(df.head())  # Print the first few rows of the DataFrame
